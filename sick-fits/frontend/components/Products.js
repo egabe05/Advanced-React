dd16728e1490dd453f371b2e17b01ba5bb706ba5
Eric Gaberik <eric.gaberik@eric.gaberik>
2022-09-18 16:17:40 +0200

    fetching and displaying products

---
+++ sick-fits/frontend/components/Products.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
+
+const allProductsQuery = gql`
+  query AllProductsQuery {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+        altText
+      }
+    }
+  }
+`;
+
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(allProductsQuery);
+  console.log(data, error, loading);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.name} product={product} />
+        ))}
+      </ProductsListStyles>
+    </div>
+  );
+}
