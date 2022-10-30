ddc40ee12e3a87863d9915dec9057034cd7149a1
Eric Gaberik <eric.gaberik@eric.gaberik>
2022-10-30 14:23:59 +0100

    config for graphql files

--- sick-fits/frontend/next.config.js
+++ sick-fits/frontend/next.config.js
@@ -1,11 +1,12 @@
-export function webpack(config) {
-  config.module.rules.push({
-    test: /\.(graphql|gql)$/,
-    exclude: /node_modules/,
-    loader: 'graphql-tag/loader',
-  });
-  return config;
-}
-export function webpackDevMiddleware(config) {
-  return config;
-}
+// do not convert to es6 module
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+
+    return config;
+  },
+};
